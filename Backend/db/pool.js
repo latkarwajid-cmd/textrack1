bd9ead123bb90ba6d3e4d720157a1f856ee9f70d
Wajid Latkar <latkarwajid@gmail.com>
2026-04-09 18:58:26 +0530

    database changes

--- Backend/db/pool.js
+++ Backend/db/pool.js
@@ -1,11 +1,11 @@
 const mysql2 = require("mysql2");
 
 const pool = mysql2.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "manager",
-  database: process.env.DB_NAME || "text_db",
-  port: Number(process.env.DB_PORT || 3306),
+  host: process.env.MYSQLHOST || "localhost",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "manager",
+  database: process.env.MYSQLDATABASE || "text_db",
+  port: Number(process.env.MYSQLPORT || 3306),
 
   ssl: {
     rejectUnauthorized: false
